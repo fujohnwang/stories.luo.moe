0bb218f5b8417426eab5f0db6487621226643c11
afoo <i@afoo.me>
2023-09-01 15:49:03 +0800

    update index page and script

---
+++ publish.sh
@@ -0,0 +1,11 @@
+currentDir=$(basename "$PWD")
+publishRepoDir="$currentDir-pages"
+
+# sync to publish folder
+rsync -zahuv --exclude-from=$HOME/afoo.me/rsync_ignore dist/ ../$publishRepoDir
+# push to github
+cd "../$publishRepoDir"
+git add .
+git ci -m "push to publish pages"
+git push
+cd "../$currentDir"
