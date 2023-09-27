2ba34715bce4e4a03eeae559b9f9588adfab61e1
afoo <i@afoo.me>
2023-09-27 22:48:45 +0800

    move best practice into this repo

--- publish.sh
+++ publish.sh
@@ -1,7 +1,12 @@
 currentDir=$(basename "$PWD")
 publishRepoDir="$currentDir-pages"
 
+echo "build site..."
 npm run build
+
+echo "build search index..."
+pagefind_extended --source dist
+
 # sync to publish folder
 rsync -zahuv --exclude-from=$HOME/afoo.me/rsync_ignore dist/ ../$publishRepoDir
 # push to github
