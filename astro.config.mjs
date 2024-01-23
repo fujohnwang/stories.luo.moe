27496caacdfa29410fe4c77e3b1c3cb33362ec31
afoo <i@afoo.me>
2024-01-23 22:12:14 +0800

    add astro icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://luomoe.github.io',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap(), icon()]
 });
