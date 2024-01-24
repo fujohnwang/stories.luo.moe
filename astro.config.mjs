6fbdb239036073fb4a66b0d3dd37f5bce711a982
afoo <i@afoo.me>
2024-01-24 23:02:29 +0800

    update astro build config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,10 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://luomoe.github.io',
-  integrations: [tailwind(), sitemap(), icon()]
+  integrations: [tailwind(), sitemap(), icon()],
+  trailingSlash: 'never',
+  build: {
+    // Example: Generate `page.html` instead of `page/index.html` during build.
+    format: 'file'
+  }
 });
